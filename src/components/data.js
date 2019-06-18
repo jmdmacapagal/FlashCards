98dbf4e0e9ac56b6318392246a208b7bfc89819e
jm macapagal <jmdmacapagal@gmail.com>
2019-06-18 13:54:12 +0800

    added flash cards data

---
+++ src/components/data.js
@@ -0,0 +1,20 @@
+
+const data = [
+    {
+        front: 'How are you?',
+        back: 'Kamusta ka?'
+    },
+    {
+        front: 'How old are you?',
+        back: 'Ilang taon kana?'
+    },
+    {
+        front: 'Where do you live',
+        back: 'Saan ka nakatira?'
+    },
+    {
+        front: 'Have you eaten?',
+        back: 'Kumain ka na ba?'
+    },
+
+]
